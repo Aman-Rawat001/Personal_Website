d83baa43b582a5043fc700863f52ed82c96ab14f
Aman Rawat <amanrawa2@gmail.com>
2021-03-21 22:30:39 +0530

    mobile website base set

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,22 +1,21 @@
 import React from "react"
 import "./HomePageStyles.css"
-
-import About from "../AboutSection/About"
-import Projects from "../projectSection/Projects"
-// import Hprojects from "./components/subComp/HProjects"
-// import MLprojects from "./components/subComp/MLprojects"
-// import WebProjects from "./components/subComp/WebProjects"
-
+import profileImage from "../images/croped-image.jpg"
 
 const HomePage = () => {
     return(
         <>
-        <div className="homepage-main-container">
-            <div className="home-inner-container">
-            <About/>
-            {/* <Projects/> */}
+            <div className="main_container">
+                <img className="profileImage" src={profileImage} alt="profileimage"/>
+                <div>
+                    <h1 className="homepage_content1">I'AM <span>WEB DEVELOPER</span></h1>
+                    <p className="homepage_content2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto quia ex tenetur autem quaerat sint, ratione nam. Dolores rerum recusandae in illum aliquam incidunt, quod eligendi porro laudantium animi neque, quaerat odit? Maxime, sequi.</p>
+                </div>
+                <div className="homepage_btn_div">
+                    <button className="homepage_btn1">More About Me</button>
+                    <button className="homepage_btn2">Portfolio</button>
+                </div>
             </div>
-        </div>
         </>
     )
 }
