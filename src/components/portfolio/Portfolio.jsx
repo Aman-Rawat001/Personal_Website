58d40cab3098d6f2b33169881932c3e6809dd790
Aman Rawat <amanrawa2@gmail.com>
2021-03-24 22:42:10 +0530

    new master branch created

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {useHistory} from "react-router-dom"
+
 import "./portfolio.css"
 import ProjectBox from "../subComp/ProjectBox"
 import Footer from "../Footer/Footer"
@@ -11,7 +11,7 @@ import img2 from "../../components/images/img2.jpg"
 import img3 from "../../components/images/img3.jpg"
 
 const Portfolio = () => {
-    const history = useHistory();
+
     return(
         <>
             
@@ -23,9 +23,6 @@ const Portfolio = () => {
             <ProjectBox img={img2} title="Web Projects" quote="Design Your Own World" hidden_link="none" link="/web-development-projects"/>
             <ProjectBox img={img3} title="ML Projects" quote="End Of The Human Race" hidden_link="none" link="/machine-learning-projects"/>
             <ProjectBox img={img1} title="Hacking Projects" quote="Break The System For Profit" hidden_link="none" link="/hacking-projects"/>
-            </div>
-            <div>
-            {/* <a style={{border: "2px solid red", position: "absolute", left: "45%", color: "black", padding: "0.5rem"}} href="/">Go Back</a> */}
             </div>
             <Footer/>
         </>
