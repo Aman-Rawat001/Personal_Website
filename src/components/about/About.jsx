bd64bd71d062d435d8e0181aaf54cf36c0576958
Aman Rawat <amanrawa2@gmail.com>
2021-03-29 22:57:40 +0530

    software understandings section setted

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import "./about.css"
 import profileImg from "../images/about_section_profile.jpg"
 import Footer from "../Footer/Footer"
@@ -9,13 +9,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
-    // library.add(faFacebookF)
+    const [showMore, setShowMore] = useState(true);
+    const showMoreHandler = (e) => {
+        if(showMore === true){
+            setShowMore(false);
+            document.getElementById("showMore").style.display = "none";
+            document.getElementById("showBtn").innerHTML = "Show More...";
+        }else{
+            setShowMore(true);
+            document.getElementById("showMore").style.display = "block";
+            document.getElementById("showBtn").innerHTML = "Show Less";
+        }
+    }
     return (
         <>
             <div className="about_title">
                 <h1>About Page</h1>
             </div>
-            <Navbar/>
+            <Navbar />
 
             <div className="about_container">
                 <img className="profileImg" src={profileImg} alt="profile image" />
@@ -24,9 +35,9 @@ const About = () => {
                     <p className="about_content2">This is <span id="name">Aman Rawat</span> from Dehradun Uttarakhand. Currently pursuing B.tech C.S.E from Graphic Era Hill University(GEHU).</p>
                     <div className="experience">
                         <h3>Working Experience In Companies</h3>
-                        <h5>@Sunfox Pvt Ltd: <span>Currently i am working with Sunfox pvt-Ltd as a Full Stack Developer since 3 months. </span></h5>
-                        <h5>@Zostel.com: <span>I have worked with Zostel.com as a react developer for just 2 months.</span></h5>
-                        <h5>@GurujiFinder.com: <span>It is basically an startUp which is founded in 2017 and i was working with them as a website ranker(SEO).</span></h5>
+                        <h5>@Sunfox Pvt Ltd: <span>Currently i am working with Sunfox pvt-Ltd as a Full Stack Developer. </span></h5>
+                        <h5>@Zostel.com: <span>I have worked with Zostel & Zoo-World as a react developer in 2020.</span></h5>
+                        <h5>@GurujiFinder.com: <span>It is basically an startUp which is founded in 2018 and i was working with them as a website ranker(SEO).</span></h5>
                     </div>
 
                     <div className="skills">
@@ -67,21 +78,39 @@ const About = () => {
                         </div>
                     </div>
 
+                    {/* external softwares */}
+                    <div className="eSoftwares">
+                        <h3>Software Understandings</h3>
+                        <p>TailwindCss</p>
+                        <p>CircleCI</p>
+                        <p>Jira</p>
+                        <div id="showMore" style={{display: "none"}}>
+                            <p>Bootstrap</p>
+                            <p>Git</p>
+                            <p>Firebase</p>
+                            <p>Canvas</p>
+                            <p>Wordpress</p>
+                            <p>GitHub</p>
+                            <p>BitBucket</p>
+                        </div>
+                        <a id="showBtn" onClick={showMoreHandler}>Show More...</a>
+                    </div>
+
                     <div className="social">
                         <h3>Social Media Handles</h3>
-                    <div className="socialMedia">
-                        <a target="_blank"  href="https://www.facebook.com/profile.php?id=100014344083429"><FontAwesomeIcon className="socialIcons" icon={faFacebook} /></a>
-                        <a target="_blank" href="https://www.instagram.com/aman_rawat0123/"><FontAwesomeIcon className="socialIcons" icon={faInstagram} /></a>
-                        <a target="_blank"  href="https://github.com/Aman-Rawat001"><FontAwesomeIcon className="socialIcons" icon={faGithub} /></a>
-                        <a target="_blank"  href="https://www.linkedin.com/in/aman-rawat-578b21193"><FontAwesomeIcon className="socialIcons" icon={faLinkedin} /></a>
-                        {/* <a href=""><FontAwesomeIcon className="socialIcons" icon={faLinkedin} /></a> */}
-                    </div>
+                        <div className="socialMedia">
+                            <a target="_blank" href="https://www.facebook.com/profile.php?id=100014344083429"><FontAwesomeIcon className="socialIcons" icon={faFacebook} /></a>
+                            <a target="_blank" href="https://www.instagram.com/aman_rawat0123/"><FontAwesomeIcon className="socialIcons" icon={faInstagram} /></a>
+                            <a target="_blank" href="https://github.com/Aman-Rawat001"><FontAwesomeIcon className="socialIcons" icon={faGithub} /></a>
+                            <a target="_blank" href="https://www.linkedin.com/in/aman-rawat-578b21193"><FontAwesomeIcon className="socialIcons" icon={faLinkedin} /></a>
+                            {/* <a href=""><FontAwesomeIcon className="socialIcons" icon={faLinkedin} /></a> */}
+                        </div>
                     </div>
 
                 </div>
             </div>
             {/* <a style={{ border: "2px solid red", position: "absolute", left: "45%", color: "black", padding: "0.5rem" }} href="/">Go Back</a> */}
-            <Footer/>
+            <Footer />
         </>
     )
 }
