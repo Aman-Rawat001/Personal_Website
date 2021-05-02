e33a84dafc1499d133144a281e6f1f1a1fe11662
Aman Rawat <amanrawa2@gmail.com>
2021-05-02 22:13:34 +0530

    download cv button set

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,41 @@
-import React from "react"
-import "./navbar.css"
+import React from "react";
+import "./navbar.css";
 
 const Navbar = () => {
-    return(
-        <>
+  return (
+    <>
+      <nav role="navigation">
+        <div id="menuToggle">
+          <input type="checkbox" />
 
-<nav role="navigation">
-  <div id="menuToggle">
+          <span></span>
+          <span></span>
+          <span></span>
 
-    <input type="checkbox" />
-    
-    <span></span>
-    <span></span>
-    <span></span>
+          <ul id="menu">
+            <a href="/">
+              <li>Home</li>
+            </a>
+            <a href="/Portfolio">
+              <li>Portfolio</li>
+            </a>
+            <a href="/projects">
+              <li>Projects</li>
+            </a>
+            <a href="/contact">
+              <li>Contact Me</li>
+            </a>
+            <a target="_blank" href="http://futuretech.epizy.com">
+              <li>Blog</li>
+            </a>
+            <a href="/download_CV">
+              <li>download CV</li>
+            </a>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-    <ul id="menu">
-      <a href="/"><li>Home</li></a>
-      <a href="/Portfolio"><li>Portfolio</li></a>
-      <a href="/projects"><li>Projects</li></a>
-      <a href="/contact"><li>Contact Me</li></a>
-      <a target="_blank" href="http://futuretech.epizy.com"><li>Blog</li></a>
-    </ul>
-  </div>
-</nav>
-        </>
-    )
-}
-
-export default Navbar
+export default Navbar;
