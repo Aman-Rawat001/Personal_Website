04f3f89187d7540284e946fd577e74e430ac1acb
Aman Rawat <amanrawa2@gmail.com>
2021-05-02 13:49:43 +0530

    popUp_form formation done

--- src/components/Home/PopUp_form/PopUp_form.js
+++ src/components/Home/PopUp_form/PopUp_form.js
@@ -1,23 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./PopUp_form.css";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import firebase from "../../../firebase";
+
 // import  from "@fortawesome/react-fontawesome"
 
 const PopUp_form = () => {
+  const db = firebase.firestore();
+
+  const [inpValue, setInpValue] = useState(" ");
+  const [submit_status, setSubmit_status] = useState("none");
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("calling");
+      setSubmit_status("flex");
+    }, 2500);
+  }, []);
+
+  const handleInpValue = (event) => {
+    setInpValue(event.target.value);
+  };
+
+  const popUp_handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (inpValue === " ") {
+      alert("Opps!, Enter your Email");
+    } else {
+      db.collection("popUp_emails")
+        .add({
+          email: inpValue,
+        })
+        .then(() => {
+          alert("Your Email has been submitted to Aman Rawat.");
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+      setInpValue("");
+      setSubmit_status("none");
+    }
+  };
+
   return (
     <>
-      <div className="popUp_bg"></div>
-      <div className="popUp_container">
+      <div className="popUp_bg" style={{ display: `${submit_status}` }}></div>
+      <div className="popUp_container" style={{ display: `${submit_status}` }}>
         <h6 className="faIcon">
           <FontAwesomeIcon icon={faCheckCircle} />
         </h6>
         <h6 className="wellcome">GREAT !</h6>
-        <h6 className="content">Wellcome, Enter Your Email</h6>
-        <h6 className="content">& Ready To Proceed.</h6>
-        <input type="email" placeholder="Enter Your Email" />
-
-        <button>Start</button>
+        <h6 className="content">Enter Your Email</h6>
+        <h6 className="content">& Ready To Proceed further...</h6>
+        <form onSubmit={popUp_handleSubmit} action="">
+          <input
+            type="email"
+            value={inpValue}
+            onChange={handleInpValue}
+            placeholder="Enter Your Email"
+          />
+          <button>Proceed</button>
+        </form>
       </div>
     </>
   );
