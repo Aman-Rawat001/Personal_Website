d64faf3065c15cbd770b87a4a187ee11773e72d2
Aman Rawat <amanrawa2@gmail.com>
2021-05-21 22:33:57 +0530

    new resume added

--- src/components/My_cv/My_cv.js
+++ src/components/My_cv/My_cv.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./My_cv.css";
 import jsPDF from "jspdf";
-import cv_pdf from "../images/Aman_rawat_cv.jpg";
+import cv_pdf from "../images/Aman_resume.jpg";
 // import Navbar from "../navbar/Navbar";
 
 const My_cv = () => {
   const pdfGenerate = () => {
     var doc = new jsPDF("landscape", "px", "a4", "false");
     doc.addImage(cv_pdf, "PNG", 65, 20, 500, 400);
+    // doc.addPage(cv_pdf, "PDF", 65, 20, 500, 400);
     doc.save("Aman_Rawat CV.pdf");
   };
   return (
