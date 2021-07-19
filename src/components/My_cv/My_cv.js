a83319a09c8938b46fe5e7644b979d9d4a90c01e
Aman Rawat <amanrawa2@gmail.com>
2021-07-18 20:57:01 -0700

    resume download link changes

--- src/components/My_cv/My_cv.js
+++ src/components/My_cv/My_cv.js
@@ -9,7 +9,7 @@ const My_cv = () => {
       {/* <Navbar /> */}
       <div className="downloadCV_container">
         <br />
-        <button><a href="https://firebasestorage.googleapis.com/v0/b/profile-website-3cadb.appspot.com/o/resume.html?alt=media&token=c7f8572b-752d-42a9-98c7-a7c15dbdfd52">Download CV</a></button>
+        <button><a href="https://firebasestorage.googleapis.com/v0/b/profile-website-3cadb.appspot.com/o/resume.html?alt=media&token=dc3d42a6-b576-4b37-a396-97fcdd08034d">Download CV</a></button>
       </div>
     </>
   );
